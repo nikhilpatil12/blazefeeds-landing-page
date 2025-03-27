6712217d7d6e6ba17746fb5aaf49d1206991bfe7
nikhilpatil12 <patil.nikhil12@outlook.com>
2025-03-27 21:05:58 +0530

    Fixes to menu

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -10,7 +10,7 @@ export const menuItems: IMenuItem[] = [
         url: "#pricing"
     },
     {
-        text: "Testimonials",
-        url: "#testimonials"
+        text: "FAQ",
+        url: "#faq"
     }
 ];
